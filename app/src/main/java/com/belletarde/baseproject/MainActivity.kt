6be6c97d8a9ace5823b84d97309273dab18d6420
Kevin Belletarde <kevin.belletarde@westwing.com.br>
2021-01-03 23:25:23 -0300

    ini nav study

--- app/src/main/java/com/belletarde/baseproject/MainActivity.kt
+++ app/src/main/java/com/belletarde/baseproject/MainActivity.kt
@@ -2,10 +2,22 @@ package com.belletarde.baseproject
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.belletarde.baseproject.ui.profile.ProfileFragment
+import com.belletarde.baseproject.ui.start.StartFragment
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), StartFragment.OnButtonClicked {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        supportFragmentManager.beginTransaction()
+            .add(R.id.mainContainer, StartFragment.newInstance())
+            .commit()
+    }
+
+    override fun buttonClicked() {
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.mainContainer, ProfileFragment.newInstance())
+            .commit()
     }
 }
